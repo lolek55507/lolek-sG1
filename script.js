b62eb33ca6408c32ab889050453c3087b1b0ff1d
lolek55507 <110234766+lolek55507@users.noreply.github.com>
2023-06-06 22:02:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body');
+const titleContainer = document.getElementsByClassName('Title-Container');
+const title = document.getElementsByClassName('title');
+let i = 1;
+
+function removeBegScreen() {
+        window.open("lolek'sG.html", "_self");
+        
+}
+function comeBack() {
+    window.open("index.html", "_self");
+    
+}
